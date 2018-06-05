5f9dc63b32bca8316dfdfae358c7b4275f12c7bf
jelf <353742991@qq.com>
2018-06-05 14:46:32 +0800

    feat: 引入IView组件库

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import iview from 'iview'
+import 'iview/dist/styles/iview.css'
 
+Vue.use(iview)
 // 生产环境是否提示
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="home">
-
+        123
+        <Input/>
     </div>
 </template>
 

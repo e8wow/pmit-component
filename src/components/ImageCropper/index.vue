4b62169893779052cb28ec866a993dd4c8c2e900
jelf <353742991@qq.com>
2018-06-07 09:38:36 +0800

    feat: 新增图片库组件。并编写完header

---
+++ src/components/ImageCropper/index.vue
@@ -0,0 +1,45 @@
+<template>
+    <div @click="handlerClick" :class="prefixCls" :style="styles">
+        <photo-gallery ref="photoGallery"/>
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Prop, Vue} from 'vue-property-decorator'
+
+    import photoGallery from '@/plugins/photoGallery/src/main.vue'
+
+    @Component({
+        components: {photoGallery}
+    })
+    export default class ImageCropper extends Vue {
+        @Prop({type: String, default: 'pmw-imageCropper'})
+        private readonly prefixCls!: string
+
+        @Prop({type: Number, default: 50})
+        private readonly width!: number
+
+        @Prop({type: Number, default: 50})
+        private readonly height!: number
+
+        private isShow: boolean = false // 是否显示图片库
+
+        private get styles() {
+            return {
+                width: `${this.width}px`,
+                height: `${this.height}px`
+            }
+        }
+
+        private handlerClick() {
+            this.$refs.photoGallery.show() // TODO 今后改成this.$photoGallery
+        }
+    }
+</script>
+
+<style lang="scss">
+    $prefixCls: 'pmw-imageCropper';
+    .#{$prefixCls} {
+        border: 1px solid #666;
+    }
+</style>
--- src/views/photoGallery/index.vue
+++ src/views/photoGallery/index.vue
@@ -1,14 +1,20 @@
 <template>
     <article>
         <h1>photo-gallery 图片库</h1>
+        <img-cropper/>
     </article>
 </template>
 
 <script lang="ts">
     import Vue from 'vue'
-    import {Component} from 'vue-property-decorator'
+    import {Component, Prop} from 'vue-property-decorator'
+
+    import imgCropper from '@/components/ImageCropper/index.vue'
 
     @Component({
+        components: {
+            imgCropper
+        },
         metaInfo: {
             title: '图片库'
         }

a035ff7e3bde919517ab388005482601e5ccb158
jelf <353742991@qq.com>
2018-06-07 13:48:27 +0800

    feat: 新增侧栏列表与列表项目组件.

---
+++ src/plugins/photoGallery/src/aside/list-item.vue
@@ -0,0 +1,26 @@
+<template>
+    <li :class="`${prefixCls}`">
+        <slot></slot>
+    </li>
+</template>
+
+<script lang="ts">
+    import {Component, Prop, Vue} from 'vue-property-decorator'
+
+    /**
+     * 图片库边栏列表容器组件
+     * @author Jelf
+     */
+    @Component({})
+    export default class PhotoGalleryAside extends Vue {
+        // 样式前缀
+        @Prop({type: String, default: 'pmw-photoGalleryAsideListItem'})
+        private readonly prefixCls!: string
+    }
+</script>
+
+<style lang="scss">
+    $prefixCls: 'pmw-photoGalleryAsideListItem';
+    .#{$prefixCls} {
+    }
+</style>
---
+++ src/plugins/photoGallery/src/aside/list.vue
@@ -0,0 +1,31 @@
+<template>
+    <div :class="prefixCls">
+        <div :class="`${prefixCls}__header`">
+            <slot name="header"></slot>
+        </div>
+        <ul :class="`${prefixCls}__body`">
+            <slot></slot>
+        </ul>
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Prop, Vue} from 'vue-property-decorator'
+
+    /**
+     * 图片库边栏列表容器组件
+     * @author Jelf
+     */
+    @Component({})
+    export default class PhotoGalleryAside extends Vue {
+        // 样式前缀
+        @Prop({type: String, default: 'pmw-photoGalleryAsideList'})
+        private readonly prefixCls!: string
+    }
+</script>
+
+<style lang="scss">
+    $prefixCls: 'pmw-photoGalleryAsideList';
+    .#{$prefixCls} {
+    }
+</style>

3474b7e981ed84325f83bb81c1d01dbb2d27695c
jelf <353742991@qq.com>
2018-07-13 17:13:16 +0800

    refactor: 去除不必要的符号

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,26 +1,26 @@
 import Vue from 'vue' // 不可删除 import Vue from 'vue' 否则此处无效
-import {MetaInfo} from 'vue-meta'
+import { MetaInfo } from 'vue-meta'
 
 declare module 'vue/types/vue' {
     interface Vue {
-        $PM: any,
+        $PM: any
         $Message: {
             info: any
             success: any
             warning: any
             error: any
             loading: any
-        },
-        $Notice: any,
+        }
+        $Notice: any
         $Modal: {
             info: any
             success: any
             warning: any
             confirm: any
-        },
-        $Loading: any,
-        $Spin: any,
-        $photoGallery: any,
+        }
+        $Loading: any
+        $Spin: any
+        $photoGallery: any
         metaInfo?: MetaInfo | (() => MetaInfo)
     }
 }

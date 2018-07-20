de5f9f12514e23a5d5c453a7683941fe67bd5da1
jelf <353742991@qq.com>
2018-07-20 11:33:37 +0800

    refactor: 由于vue-meta@1.5提供了Types所以此处需要删除

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,5 +1,4 @@
 import Vue from 'vue' // 不可删除 import Vue from 'vue' 否则此处无效
-import { MetaInfo } from 'vue-meta'
 
 declare module 'vue/types/vue' {
     interface Vue {
@@ -21,6 +20,5 @@ declare module 'vue/types/vue' {
         $Loading: any
         $Spin: any
         $photoGallery: any
-        metaInfo?: MetaInfo | (() => MetaInfo)
     }
 }
